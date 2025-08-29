5558c1b5905187842bccb23f3d542d012b513861
Jarlath <jarlath@jarlath.cc>
2025-08-28 23:37:06 -0500

    Add code for Challenge: Handle the form POST request

--- about/views.py
+++ about/views.py
@@ -9,16 +9,16 @@ from .forms import CollaborateForm
 
 def get_about(request):
 
-    about = About.objects.all().order_by("-updated_on").first()
-
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
             messages.add_message(
                 request, messages.SUCCESS,
-                'Request submitted'
+                'Collaboration request received!  I endeavour to respond within 2 working days.'
             )
+
+    about = About.objects.all().order_by("-updated_on").first()
 
     collaborate_form = CollaborateForm()
 
